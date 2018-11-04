1183263f5940943cba68eb6f91f1468833f4325d
Michael Aranda <michaelaranda0.web@gmail.com>
2018-11-04 02:11:49 +0000

    Add Event belongs_to Event Type association.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
+  belongs_to :event_type
+  
   has_many :attended_events
   has_many :attendees, through: :attended_events, source: :member
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181028173349) do
+ActiveRecord::Schema.define(version: 20181103010900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,10 @@ ActiveRecord::Schema.define(version: 20181028173349) do
     t.text     "comments"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "event_type_id"
   end
+
+  add_index "events", ["event_type_id"], name: "index_events_on_event_type_id", using: :btree
 
   create_table "members", force: :cascade do |t|
     t.string   "name"
@@ -78,6 +81,7 @@ ActiveRecord::Schema.define(version: 20181028173349) do
 
   add_foreign_key "attended_events", "events"
   add_foreign_key "attended_events", "members"
+  add_foreign_key "events", "event_types"
   add_foreign_key "reimbursements", "events"
   add_foreign_key "reimbursements", "members"
 end
