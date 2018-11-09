4eb9437eb943a753f17ee68c991cd2e70dd52a5c
Michael Aranda <michaelaranda0.web@gmail.com>
2018-11-09 23:40:02 +0000

    Display React New Event Form component (non-functioning) on custom admin Submit New Event page.

---
+++ app/javascript/bundles/components/events/MembersAutosuggest.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import Autosuggest from 'react-autosuggest';
+import Checkbox from '../checkbox';
+import { findWithAttr } from '../../helpers/arrayHelpers';
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value, members, selectedMembers) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+  let suggestions = members.filter(member => regex.test(member.name));
+  let remainingSuggestions = suggestions.filter(member => findWithAttr(selectedMembers, "name", member.name) === -1 );
+  return remainingSuggestions;
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
+
+export class MembersAutosuggest extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: [],
+      selectedMembers: []
+    };    
+  }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+  
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value, this.props.members, this.state.selectedMembers)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+  
+  onSuggestionSelectedCustom = (suggestion, suggestionValue, suggestionIndex, sectionIndex, method) => {
+    this.setState({
+      selectedMembers: [...this.state.selectedMembers, suggestionValue.suggestion],
+      value: ''
+    });
+  };
+  
+  renderSelectedMembers() {
+    return this.state.selectedMembers.map((member, i) => {
+      return (
+        <div key={i} className="selected-member-row">
+          <span className="name">{member.name}</span>
+          <Checkbox 
+            label={member.name}
+            name={`attendees[][drove]`}
+            id="event_attendee_drove"
+          />
+          <span>Drove</span>
+          
+          <input className="hidden" id="event_attendees" name={`attendees[][id]`} value={member.id} />
+        </div>
+      );
+    })
+  }
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Enter attendees",
+      value,
+      onChange: this.onChange.bind(this)
+    };
+
+    return (
+      <div>
+        <Autosuggest 
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
+          onSuggestionSelected={this.onSuggestionSelectedCustom.bind(this)}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps} />
+        <div className="selected-members-section">
+          { this.renderSelectedMembers() }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MembersAutosuggest;
---
+++ app/javascript/bundles/components/events/NewEventForm.jsx
@@ -0,0 +1,235 @@
+import $ from 'jquery'
+import React from 'react'
+import MembersAutosuggest from './MembersAutosuggest.jsx'
+import {findWithAttr} from '../../helpers/arrayHelpers';
+
+export class NewEventForm extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      name: "",
+      startTimeHour: 0,
+      startTimeMinutes: 0,
+      endTimeHour: 0,
+      endTimeMinutes: 0,
+      volunteerHours: 0,
+      drivingDistance: 0
+    }
+  }
+  
+  componentDidMount() {
+    $("form").on("keypress", function (e) {
+      if (e.keyCode == 13) {
+          return false;
+      }
+    });
+  }
+  
+  renderDateTimeSelectors() {
+    let now = new Date();
+    let currYear = now.getFullYear();
+    let currMonth = now.getMonth() + 1;
+    let currDay = now.getDate();
+    let monthLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    let minuteLabels = ["00", "15", "30", "45"];
+    
+    let years = [];
+    let months = [];
+    let days = [];
+    let start_time_hours = [];
+    let start_time_minutes = [];
+    let end_time_hours = [];
+    let end_time_minutes = [];
+    
+    for(let i = 0; i < 10; i++) {
+      years.push(<option key={i} value={currYear+i}>{currYear+i}</option>);
+    }
+    
+    for(let i = 1; i <= 12; i++) {
+      //Rails Date class is 1-12 for months, instead of 0-11 like in JavaScript
+      months.push(<option key={i} value={i}>{monthLabels[i-1]}</option>); 
+    }
+    
+    for(let i = 1; i <= 31; i++) {
+      days.push(<option key={i} value={i}>{i}</option>);        
+    }
+    
+    for(let i = 0; i < 24; i++) {
+      start_time_hours.push(<option key={i} value={i}>{i}</option>);        
+      end_time_hours.push(<option key={i} value={i}>{i}</option>);        
+    }
+    
+    for(let i = 0; i < minuteLabels.length; i++) {
+      start_time_minutes.push(<option key={i} value={i*15}>{minuteLabels[i]}</option>);        
+      end_time_minutes.push(<option key={i} value={i*15}>{minuteLabels[i]}</option>);        
+    }
+    
+    return (
+      <div>
+        <div className="field">
+          <label htmlFor="event_date">Date</label>
+          <br />
+          <select id="event_date_1i" name="event[date(1i)]">
+            {years}
+          </select>
+          <select id="event_date_2i" name="event[date(2i)]" defaultValue={currMonth}>
+            {months}
+          </select>
+          <select id="event_date_3i" name="event[date(3i)]" defaultValue={currDay}>
+            {days}
+          </select>
+        </div>
+        <div className="field">
+          <label>Start Time</label>
+          <br />
+          <input type="hidden" id="event_start_time_1i" name="event[start_time(1i)]" value={currYear} />
+          <input type="hidden" id="event_start_time_2i" name="event[start_time(2i)]" value={currMonth} />
+          <input type="hidden" id="event_start_time_3i" name="event[start_time(3i)]" value={currDay} />
+          <select 
+            id="event_start_time_4i" 
+            name="event[start_time(4i)]" 
+            value={this.state.startTimeHour}
+            onChange={this.onStartTimeHourChange.bind(this)}
+          >
+            {start_time_hours}
+          </select>
+          <select 
+            id="event_start_time_5i" 
+            name="event[start_time(5i)]" 
+            value={this.state.startTimeMinutes}
+            onChange={this.onStartTimeMinutesChange.bind(this)}
+          >
+            {start_time_minutes}
+          </select>
+        </div>
+        <div className="field">
+          <label>End Time</label>
+          <br />
+          <input type="hidden" id="event_end_time_1i" name="event[end_time(1i)]" value={currYear}/>
+          <input type="hidden" id="event_end_time_2i" name="event[end_time(2i)]" value={currMonth} />
+          <input type="hidden" id="event_end_time_3i" name="event[end_time(3i)]" value={currDay} />
+          <select 
+            id="event_end_time_4i" 
+            name="event[end_time(4i)]" 
+            value={this.state.endTimeHour}
+            onChange={this.onEndTimeHourChange.bind(this)}
+          >
+            {end_time_hours}
+          </select>
+          <select 
+            id="event_end_time_5i" 
+            name="event[end_time(5i)]"
+            value={this.state.endTimeMinutes}
+            onChange={this.onEndTimeMinutesChange.bind(this)}
+          >
+            {end_time_minutes}
+          </select>
+        </div>
+      </div>
+    );
+  }
+  
+  renderEventTypeSelector() {
+    return (
+      <select name="event[event_type_id]" defaultValue="default" onChange={this.onEventTypeSelect.bind(this)}>
+        <option disabled value="default">Select recurring event type</option>
+          {
+            this.props.eventTypes.map((eventType, i) => {
+              return (
+                <option key={i} value={eventType.id}>
+                  {eventType.name}
+                </option>
+              )
+            })
+          }
+      </select>
+    );
+  }  
+  
+  render() {
+    return (
+      <form className="add-event-form" id="new_event" action="/events" acceptCharset="UTF-8" method="post">
+        <input type="hidden" name="authenticity_token" value={this.props.authenticityToken || ''} />
+        <label>If submitting a recurring event, select from the following dropdown:</label>
+        <br />
+        {this.renderEventTypeSelector()}
+        <br />
+        <br />
+        <div className="field">
+          <label htmlFor="event_name">Name</label>
+          <br />
+          <input type="text" name="event[name]" id="event_name" value={this.state.name} />
+        </div>
+        {this.renderDateTimeSelectors()}
+        <div className="field">
+          <label htmlFor="event_volunteer_hours">Volunteer Hours</label>
+          <br />
+          <input type="text" name="event[volunteer_hours]" id="event_volunteer_hours" value={this.state.volunteerHours} />
+        </div>
+        <div className="field">
+          <label htmlFor="event_driving_distance">Driving Distance</label>
+          <br />
+          <input type="text" name="event[driving_distance]" id="event_driving_distance" value={this.state.drivingDistance} />
+        </div>
+        <div className="field">
+          <label htmlFor="event_event_summary">Event Summary</label>
+          <br />
+          <textarea name="event[event_summary]" id="event_event_summary"></textarea>
+        </div>
+        <div className="field">
+          <label htmlFor="event_comments">Comments</label>
+          <br />
+          <textarea name="event[comments]" id="event_comments"></textarea>
+        </div>
+        <label htmlFor="event_attendees">Attendees</label>
+        <MembersAutosuggest members={this.props.members} />
+        <div className="buttons-row">
+          <button onClick={() => { window.location.href='/events' }} type="button">Back</button>
+          <input type="submit" name="commit" value="Create Event" />
+        </div>
+      </form>
+    );
+  }
+  
+  onEventTypeSelect(e) {
+    let selectedEventTypeIndex = findWithAttr(this.props.eventTypes, "id", parseInt(e.target.value));
+    let selectedEventType = this.props.eventTypes[selectedEventTypeIndex];
+    
+    this.setState({
+      name: selectedEventType.name,
+      startTimeHour: selectedEventType.start_time_hour,
+      startTimeMinutes: selectedEventType.start_time_minutes,
+      endTimeHour: selectedEventType.end_time_hour,
+      endTimeMinutes: selectedEventType.end_time_minutes,
+      volunteerHours: selectedEventType.volunteer_hours,
+      drivingDistance: selectedEventType.driving_distance
+    })
+  }
+  
+  onStartTimeHourChange(e) {
+    this.setState({
+      startTimeHour: e.target.value  
+    })
+  }
+  
+  onStartTimeMinutesChange(e) {
+    this.setState({
+      startTimeMinutes: e.target.value  
+    })
+  }
+  
+  onEndTimeHourChange(e) {
+    this.setState({
+      endTimeHour: e.target.value  
+    })
+  }
+  
+  onEndTimeMinutesChange(e) {
+    this.setState({
+      endTimeMinutes: e.target.value  
+    })
+  }
+}
+
+export default NewEventForm;
