e6dcb83b770d3473db62a0da40d2a0967a7919c3
Michael Aranda <michaelaranda0.web@gmail.com>
2018-11-11 23:40:01 +0000

    Adds dependent destroy association to Event and AttendedEvents.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :event_type
   
-  has_many :attended_events
+  has_many :attended_events, dependent: :destroy
   has_many :attendees, through: :attended_events, source: :member
 end
