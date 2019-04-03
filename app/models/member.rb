ca3df7e8c825e4944238a1ae2fe9806c9dcdc8bf
Michael Aranda <michaelaranda0.web@gmail.com>
2019-04-03 04:44:41 +0000

    Adds after_initialize hook to Member model to set 0 as a default hour for volunteer hours.

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,12 +2,20 @@ class Member < ActiveRecord::Base
   has_many :attended_events
   has_many :events_attended, through: :attended_events, source: :event
   
+  after_initialize do
+    if new_record?
+      self.volunteer_hours ||= 0
+    end
+  end
+  
   def add_volunteer_hours(hours)
-    update_attributes(volunteer_hours: self.volunteer_hours + hours)
+    current_volunteer_hours = self.volunteer_hours || 0
+    update_attributes(volunteer_hours: current_volunteer_hours + hours)
   end
   
   def remove_volunteer_hours(hours)
-    new_hours_count = [0, self.volunteer_hours - hours].max
+    current_volunteer_hours = self.volunteer_hours || 0
+    new_hours_count = [0, current_volunteer_hours - hours].max
     update_attributes(volunteer_hours: new_hours_count)
   end
   
