96a093adfb00dc3221f699f915498ee7a620e13f
Michael Aranda <michaelaranda0.web@gmail.com>
2019-04-20 18:23:10 +0000

    Adds dependent: destroy relationship to event and reimbursements.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ActiveRecord::Base
   belongs_to :event_type
   
   has_many :attended_events, dependent: :destroy
+  has_many :reimbursements, dependent: :destroy
   has_many :attendees, through: :attended_events, source: :member
 end
