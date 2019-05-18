e49eaad6133c627ab25feb4e8411b861c4dc49d4
Michael Aranda <michaelaranda0.web@gmail.com>
2019-05-18 00:47:01 +0000

    Display two different types of resources on admin resources page: officer and social media.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190510185928) do
+ActiveRecord::Schema.define(version: 20190511005246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20190510185928) do
     t.string "account"
     t.string "password"
     t.text   "link"
+    t.string "category"
   end
 
   add_foreign_key "attended_events", "events"
